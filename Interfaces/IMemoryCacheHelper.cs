d0e3410c72acb3d9dc7acd40dddfcbfab6be5d75
sachinviraj <74185781+Sviraj@users.noreply.github.com>
2025-02-13 07:03:26 +0530

    commit all changes

---
+++ Interfaces/IMemoryCacheHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Interfaces
+{
+    public interface IMemoryCacheHelper
+    {
+        T CachedLong<T>(string key, Func<T> getItemCallback);
+        T Cached<T>(string key, Func<T> getItemCallback);
+    }
+}
